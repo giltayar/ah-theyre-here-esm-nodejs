7c9b815de630d78b658af8a4cb31044945ee2110
Gil Tayar <gil@tayar.org>
2020-02-18 14:05:50 +0200

    first esm test. passes!

---
+++ src/same-but-different/esm-main.mjs
@@ -0,0 +1,4 @@
+import {add, subtract} from './esm-util.mjs'
+
+console.log(add(4, 5))
+console.log(subtract(9, 4))
--- test/unit/same-but-different.unit.test.mjs
+++ test/unit/same-but-different.unit.test.mjs
@@ -9,4 +9,7 @@ describe('same-but-different (unit)', function() {
   it('cjs-main.js', async () => {
     expect(await executeModule('same-but-different/cjs-main.js')).to.eql(['9', '5'])
   })
+  it('esm-main.js', async () => {
+    expect(await executeModule('same-but-different/esm-main.mjs')).to.eql(['9', '5'])
+  })
 })
