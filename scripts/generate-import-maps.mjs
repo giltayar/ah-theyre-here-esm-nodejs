f385b4b5d2456f87f05093af74d840ea4f03092d
Gil Tayar <gil@tayar.org>
2020-02-20 10:35:26 +0200

    test importmaps support

---
+++ scripts/generate-import-maps.mjs
@@ -0,0 +1,25 @@
+import {promises as fs} from 'fs'
+import {generateImportMapForProjectPackage} from '@jsenv/node-module-import-map'
+
+async function main() {
+  await generateImportMapForProjectPackage({
+    projectDirectoryUrl: new URL('..', import.meta.url),
+    includeDevDependencies: false,
+    importMapFile: true,
+    importMapFileRelativeUrl: './dist/import-map.json',
+  })
+  const importMap = await fs.readFile('./dist/import-map.json', 'utf-8')
+  const importMapsHtml = await fs.readFile(
+    './src/03-esm-is-browser-compatible/import-maps.html',
+    'utf-8',
+  )
+  const importMapsHtmlWithImportMap = importMapsHtml.replace('$$IMPORT_MAP$$', importMap)
+
+  await fs.writeFile('./dist/import-maps.html', importMapsHtmlWithImportMap)
+}
+
+main().catch(err => {
+  console.error(err.toString())
+
+  process.exit(1)
+})
--- test/unit/03-esm-is-browser-compatible.unit.test.mjs
+++ test/unit/03-esm-is-browser-compatible.unit.test.mjs
@@ -14,7 +14,12 @@ import executeModule from './execute-module.mjs'
 
 describe('esm-is-browser-compatible (unit)', function() {
   /** @type puppeteer.Browser */ let browser
-  before(async () => (browser = await puppeteer.launch()))
+  before(
+    async () =>
+      (browser = await puppeteer.launch({
+        args: ['--enable-experimental-web-platform-features'],
+      })),
+  )
   after(() => browser.close())
 
   let staticBaseUrl
@@ -22,7 +27,12 @@ describe('esm-is-browser-compatible (unit)', function() {
   before(async () => {
     app = fastify()
     app.register(fastifyStatic, {
-      root: path.join(__dirname, '../../src/03-esm-is-browser-compatible'),
+      root: path.join(__dirname, '../../'),
+    })
+    app.register(fastifyStatic, {
+      decorateReply: false,
+      prefix: '/dist/node_modules',
+      root: path.join(__dirname, '../../node_modules'),
     })
     staticBaseUrl = await app.listen()
   })
@@ -32,9 +42,23 @@ describe('esm-is-browser-compatible (unit)', function() {
     const page = await browser.newPage()
     let consoleLogs = []
     page.on('console', msg => consoleLogs.push(msg.text()))
-    await page.goto(new URL(`${staticBaseUrl}/imports-module.html`, import.meta.url))
+    await page.goto(
+      new URL(
+        `${staticBaseUrl}/src/03-esm-is-browser-compatible/imports-module.html`,
+        import.meta.url,
+      ),
+    )
 
     expect(consoleLogs).to.eql(['Hello, world'])
+  })
+
+  it('browsers support bare-specifiers with importmap', async () => {
+    const page = await browser.newPage()
+    let consoleLogs = []
+    page.on('console', msg => consoleLogs.push(msg.text()))
+    await page.goto(new URL(`${staticBaseUrl}/dist/import-maps.html`, import.meta.url))
+
+    expect(consoleLogs).to.eql([(await import('mjs-example')).hello])
   })
 
   it('node supports same esm code as browsers', async () => {
